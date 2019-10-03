6ddc0b62c01bf730103d3ac8d21c99b0e40e26d6
aryandevops <deadsoul07@gmail.com>
2019-10-03 16:36:22 +0530

    Add project files.

---
+++ Adding2Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adding2Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your 1st Number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter your 2nd Number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Sum is equal to " + (num1 + num2));
+            
+
+            int num3 = 10;
+            Console.WriteLine(num3);
+            Console.ReadLine();
+
+
+        }
+    }
+}
